799b33b888d8c6ca3dea114e0e40fae19acde130
ThomMoerman <thomas.moerman.7@gmail.com>
2023-04-06 10:30:36 +0200

    Exercice 2.1

---
+++ Exercice 02/2.1.js
@@ -0,0 +1,10 @@
+const prompt=require("prompt-sync")({sigint:true}); 
+
+let age = prompt("Enter your age : ");
+
+if (age >= 18){
+    console.log('You are an Adult')
+}else{
+        console.log("You are not yet an adult");
+    
+}
