3519e81553f6f85a6988e8a0b929a56cb783d825
ThomMoerman <thomas.moerman.7@gmail.com>
2023-04-06 11:30:44 +0200

    Exercice 2.3

---
+++ Exercice 02/2.3.js
@@ -0,0 +1,23 @@
+// Exécution 100fois boucle while
+
+// let num = 0;
+
+// while (num <= 100) {
+//     if (num % 2 === 0) {
+//         console.log(num);
+//         num += 1;
+//     } else {
+//         num += 1;
+//     }
+// }
+
+// Exécution 50 fois boucle for 
+
+let num = 2;
+
+for (i=0; i<50; i++){
+    if (num % 2 === 0){
+        console.log(num);
+        num+=2;
+    }
+}
