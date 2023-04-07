a2be94f03b3d2394b765f3d9b764abfef5dda8df
ThomMoerman <thomas.moerman.7@gmail.com>
2023-04-07 09:34:19 +0200

    Exercice 3.0

---
+++ Exercice 03/3.0.js
@@ -0,0 +1,7 @@
+// Create an array that contains every learner from the class (you can find the complete list on the Discord server). Display each name in the console using a for ... of ... loop.
+
+let arr = ["Abel Fortunati", "Alexandra Petit", "Anthony Denayer", "Benjamin Mayeur", "Bruno Lafont", "Ethan Dias", "Ismaël Mesmoudi", "Joddie Addis", "Julien Arnould", "Louka Cado", "Loic Lion","Marine Fassin","Nikko Di Bernardo", "Steve Grard", "Sylvain Jacobs", "Thomas Moerman"];
+
+for(let member of arr){
+    console.log(member);
+}
