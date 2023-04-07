5f9e0c712e1ad71d311c00519b78bba9e1d4b724
ThomMoerman <thomas.moerman.7@gmail.com>
2023-04-07 09:59:39 +0200

    Exercice 3.2

---
+++ Exercice 03/3.2.js
@@ -0,0 +1,17 @@
+// Write a program that will calculate the average value of a given array.
+
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [100, 101, 102];
+var result1 = 0;
+var result2 = 0;
+
+for (let i = 0; i < arr1.length; i += 1) {
+     result1+=arr1[i];
+}
+
+for (let i = 0; i < arr2.length; i += 1) {
+    result2+=arr2[i];
+}
+
+console.log('The average of all elements in the first array is : '+result1/arr1.length);
+console.log('The average of all elements in the second array is : '+result2/arr2.length);
