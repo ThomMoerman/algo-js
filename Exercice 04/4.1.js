d2c8ce0a76add9b277ace2edc6a3713f369852e3
ThomMoerman <thomas.moerman.7@gmail.com>
2023-04-07 10:26:10 +0200

    Exercice 4.1

---
+++ Exercice 04/4.1.js
@@ -0,0 +1,17 @@
+// Create a function named calcSurface that takes the length and width of a rectangle and returns its surface. Then create a program that asks the length and width of a rectangle to the user then displays the surface of that rectangle. That program must use the function you created.
+
+// Write a documentation for the calcSurface function.
+
+function calcSurface(lengthRec, widthRec) {
+    let surface = lengthRec*widthRec
+    return surface;
+  }
+
+const prompt = require("prompt-sync")({
+    sigint: true
+});
+
+let lengthUser = prompt("Enter a length for a rectangle : ");
+let widthUser = prompt("Enter a width for the same rectangle : ");
+
+console.log('The surface of the rectangle you\'ve created is equal to : ' +calcSurface(lengthUser,widthUser))
